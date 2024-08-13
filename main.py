e1df2b70d99917d1d7317560f990c79d5e9d7c30
florenciabang <florenciabang@gmail.com>
2024-08-13 14:21:42 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""print ("Hola Mundo")
+print ("Bienvenidos a S6")"""
+
+"""num1 y num2 son variable entonces no necesitan comillas"""
+num1 = 5
+num2 = 2
+
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+print (num1 % num2)
+
---
+++ reconocer_errores.py
@@ -0,0 +1,26 @@
+# código 1
+"""print("Hola, mundo!")
+
+num = 10
+
+print("El número es", num)
+
+suma = 5 + 3
+
+print(suma)"""
+
+
+# código 2
+"""nombre = "Juan"
+
+apellido = "Pérez"
+
+nombre_completo = nombre + apellido
+
+print(nombre_completo)"""
+
+
+# código 3
+edad = int (input("¿Cuántos años tienes?"))
+
+print("El año que viene tendrás", edad + 1, "años. 1")
